440ec82552326e31153e2695db2910a89f987223
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-07-05 16:17:05 +0530

    fix blog page design with API

--- nextjs-project/pages/api/blogs.js
+++ nextjs-project/pages/api/blogs.js
@@ -1,9 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "fs";
 
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data);
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  try {
+    let data = await fs.promises.readdir("blogdata");
+    let allBlogs = [];
+
+    for (let i = 0; i < data.length; i++) {
+      const item = data[i];
+      console.log(item);
+      const myFile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+      console.log(myFile);
+      allBlogs.push(JSON.parse(myFile));
+    }
+
+    res.status(200).json(allBlogs);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
--- nextjs-project/pages/blog.js
+++ nextjs-project/pages/blog.js
@@ -1,44 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "../styles/Blog.module.css";
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((parsed) => {
+        console.log(parsed);
+        setBlogs(parsed);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className={styles.blogItem}>
-          <Link href={"blogpost/learn-javascript"}>
-            {" "}
-            <h3>How to Learn Javascript?</h3>
-            <p>
-              Javascript is the language used to design logic for the web page
-            </p>
-          </Link>
-        </div>
-        <div className={styles.blogItem}>
-          <Link href={"blogpost/learn-javascript"}>
-            <h3>How to Learn Javascript?</h3>
-            <p>
-              Javascript is the language used to design logic for the web page
-            </p>
-          </Link>
-        </div>
-        <div className={styles.blogItem}>
-          <Link href={"blogpost/learn-javascript"}>
-            <h3>How to Learn Javascript?</h3>
-            <p>
-              Javascript is the language used to design logic for the web page
-            </p>
-          </Link>
-        </div>
-        <div className={styles.blogItem}>
-          <Link href={"blogpost/learn-javascript"}>
-            <h3>How to Learn Javascript?</h3>
-            <p>
-              Javascript is the language used to design logic for the web page
-            </p>
-          </Link>
-        </div>
+        {blogs.map((blogitem) => {
+          return (
+            <div key={blogitem.id} className={styles.blogItem}>
+              <Link href={`blogpost/${blogitem.slug}`}>
+                <h3 className={styles.blogItemh3}>{blogitem.title}</h3>
+                <p className={styles.blogItemp}>
+                  {blogitem.content.substr(0, 140)}...
+                </p>
+              </Link>
+            </div>
+          );
+        })}
       </main>
     </div>
   );
