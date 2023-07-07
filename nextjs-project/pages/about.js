2a21a1c19f4224c0e20760a771b2947fe27b4569
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-07-07 09:23:28 +0530

    Designing the About Page

--- nextjs-project/pages/about.js
+++ nextjs-project/pages/about.js
@@ -1,9 +1,62 @@
 import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is About Page</h1>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>About Hunting Coder</h1>
+        <h2>Introduction</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+          laudantium corporis, veritatis eveniet molestiae autem dolorum
+          repudiandae debitis, similique incidunt, architecto porro
+          necessitatibus temporibus iusto expedita quia soluta harum ipsam!
+          Minima nostrum reprehenderit deserunt vel, excepturi saepe fugit modi
+          soluta at tenetur quia architecto animi tempore minus autem, quidem
+          reiciendis accusamus exercitationem culpa? Dolorem officia est
+          deleniti nam? Saepe, dolorem deleniti fuga minima molestias dicta
+          assumenda voluptas, delectus animi iusto aliquam accusantium corrupti
+          minus vero debitis nobis odit totam distinctio. Ab maxime quaerat ex
+          suscipit mollitia repellendus ea repellat, omnis earum sapiente.
+          Voluptates asperiores accusantium consequuntur repellendus officiis
+          ipsa quae.
+        </p>
+
+        <h2>About Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+          laudantium corporis, veritatis eveniet molestiae autem dolorum
+          repudiandae debitis, similique incidunt, architecto porro
+          necessitatibus temporibus iusto expedita quia soluta harum ipsam!
+          Minima nostrum reprehenderit deserunt vel, excepturi saepe fugit modi
+          soluta at tenetur quia architecto animi tempore minus autem, quidem
+          reiciendis accusamus exercitationem culpa? Dolorem officia est
+          deleniti nam? Saepe, dolorem deleniti fuga minima molestias dicta
+          assumenda voluptas, delectus animi iusto aliquam accusantium corrupti
+          minus vero debitis nobis odit totam distinctio. Ab maxime quaerat ex
+          suscipit mollitia repellendus ea repellat, omnis earum sapiente.
+          Voluptates asperiores accusantium consequuntur repellendus officiis
+          ipsa quae.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
+          laudantium corporis, veritatis eveniet molestiae autem dolorum
+          repudiandae debitis, similique incidunt, architecto porro
+          necessitatibus temporibus iusto expedita quia soluta harum ipsam!
+          Minima nostrum reprehenderit deserunt vel, excepturi saepe fugit modi
+          soluta at tenetur quia architecto animi tempore minus autem, quidem
+          reiciendis accusamus exercitationem culpa? Dolorem officia est
+          deleniti nam? Saepe, dolorem deleniti fuga minima molestias dicta
+          assumenda voluptas, delectus animi iusto aliquam accusantium corrupti
+          minus vero debitis nobis odit totam distinctio. Ab maxime quaerat ex
+          suscipit mollitia repellendus ea repellat, omnis earum sapiente.
+          Voluptates asperiores accusantium consequuntur repellendus officiis
+          ipsa quae.
+        </p>
+      </div>
     </div>
   );
 };
